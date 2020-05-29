f3ec3f39f88d5faa2ec86fd598b7627537dbbf27
mffguimaraes <matheusffguimaraes@gmail.com>
2020-05-28 22:20:48 -0300

    Vai bola.

--- app/src/main/java/br/ucs/android/exemplocanvas/CanvasView.java
+++ app/src/main/java/br/ucs/android/exemplocanvas/CanvasView.java
@@ -16,10 +16,23 @@ public class CanvasView extends View {
     private Canvas mCanvas;
     Context context;
 
+
+    // Record current ball horizontal ordinate.
+    private float currX = 100;
+
+    // Record current ball vertical ordinate
+    private float currY = 100;
+
+    // This is the circle color.
+    private int circleColor = Color.GREEN;
+
+    private int mX;
+    private int mY;
+
     public CanvasView(Context c, AttributeSet attrs) {
         super(c, attrs);
         context = c;
-  }
+    }
 
     // override onSizeChanged
     @Override
@@ -39,35 +52,82 @@ public class CanvasView extends View {
         // Retângulo
         Paint p1 = new Paint();
         p1.setColor(Color.BLUE);
-        canvas.drawRect(100, 100, 200,250,p1);
+        canvas.drawRect(100, 100, 200, 250, p1);
 
         // Círculos
         Paint p2 = new Paint();
         p2.setColor(Color.YELLOW);
-        canvas.drawCircle(300, 300, 35,p2);
-        p2.setColor(Color.DKGRAY);
-        canvas.drawCircle(600, 300, 80,p2);
+        canvas.drawCircle(currX, currY, 35, p2);
+
+        p2.setColor(circleColor);
+        canvas.drawCircle(600, 300, 80, p2);
 
         // Texto
         Paint p3 = new Paint();
         p3.setTextSize(48f);
         p3.setColor(Color.GREEN);
-        canvas.drawText("Texto no canvas",100, 400, p3);
+        canvas.drawText("Texto no canvas", 100, 400, p3);
 
-        // Linhas
-        Paint p4 = new Paint();
-        p4.setStrokeWidth(5f);
-        p4.setColor(Color.RED);
-        canvas.drawLine(100,500, 400, 550,p4);
-        canvas.drawLine(100,500, 420, 650,p4);
-        canvas.drawLine(100,500, 500, 800,p4);
-
+//        // Linhas
+//        Paint p4 = new Paint();
+//        p4.setStrokeWidth(5f);
+//        p4.setColor(Color.RED);
+//        canvas.drawLine(100,500, 400, 550,p4);
+//        canvas.drawLine(100,500, 420, 650,p4);
+//        canvas.drawLine(100,500, 500, 800,p4);
 
 
     }
 
-    public void clearCanvas() {
-        invalidate();
+    public float getCurrX() {
+        return currX;
     }
 
+    public void setCurrX(float currX) {
+        this.currX = currX;
+    }
+
+    public float getCurrY() {
+        return currY;
+    }
+
+    public void setCurrY(float currY) {
+        this.currY = currY;
+    }
+
+    public int getCircleColor() {
+        return circleColor;
+    }
+
+    public void setCircleColor(int circleColor) {
+        this.circleColor = circleColor;
+    }
+
+    public void updateY(int y) {
+        if (currY + y > 0 && currY + y < mY) {
+            this.currY += y;
+        }
+    }
+
+    public void updateX(int x) {
+        if (currX + x > 0 && currX + x < mX) {
+            this.currX += x;
+        }
+    }
+
+    public int getmX() {
+        return mX;
+    }
+
+    public void setmX(int mX) {
+        this.mX = mX;
+    }
+
+    public int getmY() {
+        return mY;
+    }
+
+    public void setmY(int mY) {
+        this.mY = mY;
+    }
 }
--- app/src/main/java/br/ucs/android/exemplocanvas/MainActivity.java
+++ app/src/main/java/br/ucs/android/exemplocanvas/MainActivity.java
@@ -2,18 +2,83 @@ package br.ucs.android.exemplocanvas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import static android.content.Context.SENSOR_SERVICE;
+
+public class MainActivity extends Activity {
 
     private CanvasView customCanvas;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Point point = new Point();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        WindowManager wm = getWindowManager();
+        Display d = wm.getDefaultDisplay();
+        d.getSize(point);
+
+
         customCanvas = (CanvasView) findViewById(R.id.signature_canvas);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(new AccelerometerSensor(), mAccelerometer,
+                SensorManager.SENSOR_DELAY_GAME);
+
+        customCanvas.setmX(point.x);
+        customCanvas.setmY(point.y);
+    }
+
+    private class AccelerometerSensor implements  SensorEventListener {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float currY = event.values[0];
+            float currX =event.values[1];
+            float currZ = event.values[2];
+
+
+            if(currX > 2 || currX < -2){
+                if(customCanvas.getCurrX() > 0 && customCanvas.getCurrX() < point.x){
+                    customCanvas.updateX((int)currX);
+                }
+            }
+
+            if(currY > 2.5 || currY < -2.5){
+//                if(customCanvas.getCurrY() > 0 && customCanvas.getCurrY() < point.y)
+                customCanvas.updateY((int)currY);
+                System.out.println(String.format("%.2f", currY));
+            }
+
+
+            // Set circle color to blue.
+            customCanvas.setCircleColor(Color.BLUE);
+
+            // Notify drawCircleView to redraw. This will invoke DrawBallView's onDraw() method.
+            customCanvas.invalidate();
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+
+
     }
 }
